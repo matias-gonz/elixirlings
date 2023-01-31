a39d4e1219a73db88c4e38c0925eb64674b5dd0c
matias-gonz <maigonzalez@fi.uba.ar>
2023-01-31 17:20:45 -0300

    Add GeneralStore.enchant_items

--- exercises/general_store.ex
+++ exercises/general_store.ex
@@ -14,6 +14,19 @@ defmodule GeneralStore do
     filter_items(rest, magic_state, filtered)
   end
 
+  @enchanter_name "Edwin"
+  def enchant_for_sale(item) do
+    %{
+      title: "#{@enchanter_name}'s #{item.title}",
+      price: item.price * 3,
+      magic: true
+    }
+  end
+
+  def enchant_items(items) do
+    Enum.map(items, &enchant_for_sale/1)
+  end
+
   def test_data do [
     %{title: "Longsword", price: 50, magic: false}, %{title: "Healing Potion", price: 60, magic: true}, %{title: "Rope", price: 10, magic: false},
     %{title: "Dragon's Spear", price: 100, magic: true},
