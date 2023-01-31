d57429ef60786d96c2567c9edca75479ebe6402e
matias-gonz <maigonzalez@fi.uba.ar>
2023-01-31 13:50:53 -0300

    Update Tax

--- exercises/tax.ex
+++ exercises/tax.ex
@@ -11,4 +11,21 @@ defmodule Tax do
     def income_tax(salary)do
         salary*0.15
     end
+
+    def net_wage(salary) do
+      salary - income_tax(salary)
+    end
+
+    def itax() do
+        user_input = IO.gets "Your income: "
+
+        _result = case Integer.parse(user_input) do
+            :error ->
+                "Invalid ability score: #{user_input}"
+            {salary, _} ->
+                tax = income_tax(salary)
+                net_wage = net_wage(salary)
+                "Your income tax is #{tax} and your net wage is #{net_wage}"
+        end
+    end
 end
