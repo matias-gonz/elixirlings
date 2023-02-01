6c206f9f0c321698d34bd5980e7197a0df5e5ca5
matias-gonz <maigonzalez@fi.uba.ar>
2023-02-01 19:47:29 -0300

    Add application skeleton for Auction

---
+++ auction_umbrella/apps/auction/lib/auction.ex
@@ -0,0 +1,17 @@
+defmodule Auction do
+  alias Auction.{Item, FakeRepo}
+
+  @repo FakeRepo
+
+  def list_item do
+    @repo.all(Item)
+  end
+
+  def get_item(id) do
+    @repo.get(Item, id)
+  end
+
+  def get_item_by(attrs) do
+    @repo.get_by(Item, attrs)
+  end
+end
