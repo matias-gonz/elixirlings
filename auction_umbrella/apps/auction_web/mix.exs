9560e5be7f77432c2f760e7459d01a5c8c76fea3
matias-gonz <maigonzalez@fi.uba.ar>
2023-02-02 17:15:11 -0300

    Add PostgreSQL db

--- auction_umbrella/apps/auction_web/mix.exs
+++ auction_umbrella/apps/auction_web/mix.exs
@@ -49,7 +49,10 @@ defmodule AuctionWeb.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:auction, in_umbrella: true}
+      {:auction, in_umbrella: true},
+      {:ecto_sql, ">= 0.0.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_ecto, ">= 0.0.0"}
     ]
   end
 
--- auction_umbrella/config/config.exs
+++ auction_umbrella/config/config.exs
@@ -47,3 +47,12 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+config :auction, ecto_repos: [Auction.Repo]
+
+config :auction, Auction.Repo,
+  database: "auction",
+  username: "phoenix",
+  password: "pass",
+  hostname: "localhost",
+  port: "5432"
