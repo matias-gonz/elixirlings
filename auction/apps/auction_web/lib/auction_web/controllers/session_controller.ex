009ba9a3b8df4a987a7db8ea75fc48809c4d0349
matias-gonz <maigonzalez@fi.uba.ar>
2023-02-03 14:42:11 -0300

    Add Authenticator

--- auction/apps/auction_web/lib/auction_web/controllers/session_controller.ex
+++ auction/apps/auction_web/lib/auction_web/controllers/session_controller.ex
@@ -4,4 +4,18 @@ defmodule AuctionWeb.SessionController do
   def new(conn, _params) do
     render(conn, "new.html")
   end
+
+  def create(conn, %{"user" => %{"username" => username, "password" => password}}) do
+    case Auction.get_user_by_username_and_password(username, password) do
+      %Auction.User{} = user ->
+        conn
+        |> put_session(:user_id, user.id)
+        |> put_flash(:info, "Welcome back #{username}!")
+        |> redirect(to: Routes.item_path(conn, :index))
+      _ ->
+        conn
+        |> put_flash(:error, "Wrong credentials")
+        |> render("new.html")
+    end
+  end
 end
